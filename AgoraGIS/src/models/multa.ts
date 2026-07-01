1027658cbf1a6e12ff959fd09609b46c21cf92c0
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-01 09:16:05 -0600

    feat: se agrego mapeo entidad de multa

---
+++ AgoraGIS/src/models/multa.ts
@@ -0,0 +1,9 @@
+export interface multa {
+    id_multa: number;
+    motivo: string;
+    monto: number;
+    fecha_emision: Date;
+    estado: string;
+    id_asignacion: number;
+    id_usuario: number;
+}
