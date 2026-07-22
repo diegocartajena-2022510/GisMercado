9328fe102d7d572fab0a402a165707867828914a
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-22 11:43:42 -0600

    feat: se hizo repository

--- AgoraGIS/src/data/vendedorRepository.ts
+++ AgoraGIS/src/data/vendedorRepository.ts
@@ -2,6 +2,7 @@ import {readFile, writeFile} from "fs/promises";
 import {Vendedor} from "../models/vendedor";
 
 export class VendedorRepository{
+    private ruta = "./src/data/vendedor.json";
 
     async listarVendedores():Promise<Vendedor[]>{
         try{
@@ -25,38 +26,82 @@ export class VendedorRepository{
             console.error("Error al agregar un nuevo vendedor:", error);
         }
     }
-    async eliminarVendedor(id_vendedor:number):Promise<void>{
-        try{
+    
+    async actualizarVendedores(vendedorActualizado: Vendedor): Promise<boolean> {
+
+        try {
+
             const vendedores = await this.listarVendedores();
-            const vendedoresActualizados = vendedores.filter(vendedor => vendedor.id_vendedor !== id_vendedor);
-            await writeFile("src/data/vendedor.json", JSON.stringify(vendedoresActualizados, null, 2), "utf-8");
-        }catch(error){
-            console.error("Error al eliminar un vendedor:", error);
+
+            const indice = vendedores.findIndex(
+                Vendedor => Vendedor.id_vendedor === vendedorActualizado.id_vendedor
+            );
+
+            if (indice === -1) {
+
+                return false;
+
+            }
+
+            vendedores[indice] = vendedorActualizado;
+
+            await writeFile(
+                this.ruta,
+                JSON.stringify(vendedores, null, 2)
+            );
+
+            return true;
+
+        } catch (error) {
+
+            console.log("Error al actualizar.");
+
+            return false;
+
         }
+
     }
-    async buscarVendedorPorId(id_vendedor:number):Promise<Vendedor | null>{
-        try{
-            const vendedores = await this.listarVendedores();
-            const vendedorEncontrado = vendedores.find(vendedor => vendedor.id_vendedor === id_vendedor);
-            return vendedorEncontrado || null;
-        }catch(error){
-            console.error("Error al buscar un vendedor por ID:", error);
-            return null;
-        }
+    async obtenerVendedoresPorId(id: number): Promise<Vendedor | undefined> {
+
+        const usuarios = await this.listarVendedores();
+
+        return usuarios.find(usuario => usuario.id_vendedor === id);
+
     }
 
-    async actualizarVendedor(id_vendedor:number, vendedorActualizado:Vendedor):Promise<void>{
-        try{
-            const vendedores = await this.listarVendedores();
-            const index = vendedores.findIndex(vendedor => vendedor.id_vendedor === id_vendedor);
-            if(index !== -1){
-                vendedores[index] = vendedorActualizado;
-                await writeFile("src/data/vendedor.json", JSON.stringify(vendedores, null, 2), "utf-8");
-            }else{
-                console.error("Vendedor no encontrado para actualizar");
+
+    async eliminarVendedor(id: number): Promise<boolean> {
+
+        try {
+
+            const usuarios = await this.listarVendedores();
+
+            const nuevosUsuarios = usuarios.filter(
+                usuario => usuario.id_vendedor !== id
+            );
+
+            if (usuarios.length === nuevosUsuarios.length) {
+
+                return false;
+
             }
-        }catch(error){
-            console.error("Error al actualizar un vendedor:", error);
+
+            await writeFile(
+                this.ruta,
+                JSON.stringify(nuevosUsuarios, null, 2)
+            );
+
+            return true;
+
+        } catch (error) {
+
+            console.log("Error al eliminar.");
+
+            return false;
+
         }
+
     }
+
+    
 }
