7d4e25570563f1d59c0797ac1867b917dddec772
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-25 15:52:43 -0600

    refactor: se agrego metodos de eliminar

--- AgoraGIS/src/api/router.ts
+++ AgoraGIS/src/api/router.ts
@@ -36,8 +36,45 @@ router.post('/vendedor',async (req:Request, res: Response)=>{
         await service.agregarVendedores(NuevoVendedor);
         res.status(201).json({mensaje: "vendedor registrado"});
     }catch(error:any){
-        console.error("❌ ERROR REAL DE MYSQL:", error);
         res.status(500).json({mensaje: error.message});
+    }
+});
+
+//editar vendedor
+
+router.put('/vendedor/:id',async (req:Request,res: Response)=>{
+    try{
+        const id= Number(req.params.id);
+        const editarVendedor=req.body;
+        const vendedorExistente= await service.BuscarVendedoresPorId(id);
+        if(!vendedorExistente){
+            res.status(404).json({mensaje: "vendedor no encontrado"})
+            return;
+        }
+
+        await service.editarVendedores(id,editarVendedor);
+        res.status(200).json({mensaje: "vendedor actualizado"});
+    }catch(error:any){
+        console.error("error al actualizar", error);
+        res.status(500).json("mensaje: error.message")
+    }
+})
+
+// eliminar vendedor
+
+router.delete('/vendedor/:id', async (req: Request, res: Response)=>{
+    try{
+        const id=Number(req.params.id);
+
+        const vendedorExistente= await service.BuscarVendedoresPorId(id);
+        if(!vendedorExistente){
+            res.status(404).json({mensaje: "vendedor no encontrado"});
+            return;
+        }
+        await service.eliminarVendedores(id);
+        res.status(202).json({mensaje: "vendedor eliminado correctamente"});
+    }catch(error:any){
+        res.status(500).json({mensaje:error.message})
     }
 });
 
