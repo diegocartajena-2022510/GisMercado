c266bc67e648c256f9e7ec85439b60fc9b416e4e
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-25 15:01:22 -0600

    refactor: se cambio metodo

--- AgoraGIS/src/api/router.ts
+++ AgoraGIS/src/api/router.ts
@@ -1,137 +1,44 @@
-import { IncomingMessage, ServerResponse } from "http";
-import { VendedorService } from "../service/vendedorService";
-import { rolUsuario } from "../models/enums";
+import{Router,Request,Response} from 'express';
+import { VendedorService } from '../service/vendedorService';
 
+const router= Router();
 const service= new VendedorService();
 
-export async function router(req: IncomingMessage, res: ServerResponse) {
-    res.setHeader("Content-Type", "application/json");
-    const url = req.url ?? "";
-    const metodo = req.method ?? "";
-
+// listar vendedores
+router.get('/vendedor',async (req:Request, res:Response)=>{
     try{
-        // mostrar vendedores
-        if(metodo==="GET" && url==="/vendedores"){
-            const vendedores = await service.listarVendedores();
-            res.writeHead(200);
-            res.end(JSON.stringify(vendedores));
-            return;
-        }
-
-        // mosrtar por id
-        if(metodo==="GET" && url.startsWith("/vendedores/")){
-            const id= Number(url.split("/")[2]);
-            const vendedor= await service.buscarVendedorPorId(id);
-
-            if(!vendedor){
-                res.writeHead(404);
-                res.end(JSON.stringify({
-                    mensaje:"usuario no encontrado"
-                }));
-                return;
-            }
-            res.writeHead(200);
-            res.end(JSON.stringify(vendedor));
-        }
-
-        // agregar vendedor
-        if(metodo ==="POST"&& url==="/vendedores"){
-            let body="";
-            req.on("data", (chunk)=>{
-                body += chunk;
-            });
-            req.on("end",async ()=>{
-                try{
-                    const vendedor=JSON.parse(body);
-                    await service.agegarVendedor(vendedor);
-                    res.writeHead(201);
-                    res.end(JSON.stringify({
-                        mensaje: "usuario agregado"
-                    }));
-                }catch(error){
-                    res.writeHead(400);
-                    res.end(JSON.stringify({
-                        mensaje: (error as Error).message
-                    }));
-                }
-            });
-            return;
-        }
-        // actualizar vendedor
-        if (metodo === "PUT" && url.startsWith("/vendedores/")) {
-
-            let body = "";
-
-            req.on("data", chunk => {
-
-                body += chunk;
-
-            });
-
-            req.on("end", async () => {
-
-                try {
-
-                    const vendedor = JSON.parse(body);
-
-                    vendedor.id = Number(url.split("/")[2]);
-
-                    await service.actualizarVendedor(vendedor);
-
-                    res.writeHead(200);
-
-                    res.end(JSON.stringify({
-                        mensaje: "vendedor actualizado"
-                    }));
-
-                } catch (error) {
-
-                    res.writeHead(400);
-
-                    res.end(JSON.stringify({
-                        mensaje: (error as Error).message
-                    }));
-
-                }
-
-            });
-
-            return;
-
-        }
-        // eliminar vendedor
-        if (metodo === "DELETE" && url.startsWith("/vendedores/")) {
-
-            const id = Number(url.split("/")[2]);
-
-            await service.eliminarVendedor(id);
-
-            res.writeHead(200);
-
-            res.end(JSON.stringify({
-                mensaje: "vendedor eliminado"
-            }));
-
-            return;
-
-        }
-
-        // Ruta no encontrada
-        res.writeHead(404);
-
-        res.end(JSON.stringify({
-            mensaje: "Ruta no encontrada"
-        }));
-
-    } catch (error) {
-
-        res.writeHead(500);
-
-        res.end(JSON.stringify({
-            mensaje: (error as Error).message
-        }));
-
+        const vendedores=await service.listarVendedores();
+        res.status(200).json(vendedores);
+    }catch(error: any){
+        res.status(500).json({mensaje: error.message});
     }
-    
-}
+});
+// buscar por id
+router.get('/vendedor/:id',async (req: Request, res:Response)=>{
+   try{
+        const id=Number(req.params.id);
+        const vendedores=await service.BuscarVendedoresPorId(id);
+        if(!vendedores){
+            res.status(404).json({mensaje: "vendedor no encontrado"});
+            return;
+        }
+        res.status(200).json(vendedores);
+   }catch(error: any){
+        res.status(500).json({mensaje: error.message});
+   }
 
+   
+});
+// agregar
+router.post('/vendedor',async (req:Request, res: Response)=>{
+    try{
+        const NuevoVendedor=req.body;
+        await service.agregarVendedores(NuevoVendedor);
+        res.status(201).json({mensaje: "vendedor registrado"});
+    }catch(error:any){
+        console.error("❌ ERROR REAL DE MYSQL:", error);
+        res.status(500).json({mensaje: error.message});
+    }
+});
+
+export default router;
