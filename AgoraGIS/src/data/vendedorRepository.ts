93f2b0c77cf3d4500da8d4378627793df3f53969
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-26 18:54:51 -0600

    fix: se arreglo metodo listar

--- AgoraGIS/src/data/vendedorRepository.ts
+++ AgoraGIS/src/data/vendedorRepository.ts
@@ -3,61 +3,55 @@ import {Vendedor} from "../models/vendedor";
 
 export class VendedorRepository{
     async listar(): Promise<Vendedor[]>{
-        const [rows]: any= await db.query('CALL sp_vendedor_listar(?)');
+        const [rows]: any= await db.query('CALL sp_vendedor_listar()');
         return rows[0] as Vendedor[];
     }
 
-//buscar
-    async buscarPorId(id: number): Promise<Vendedor | null>{
-        const [rows]: any= await db.query('CALL sp_vendedor_buscar(?)',[id]);
-        const resultado= rows[0];
+  //buscar
+      async buscarPorId(id: number): Promise<Vendedor | null>{
+          const [rows]: any= await db.query('CALL sp_vendedor_buscar(?)',[id]);
+          const resultado= rows[0];
 
-        if(!resultado || resultado.length===0){
-            return null;
-        }
-        return resultado[0] as Vendedor;
+          if(!resultado || resultado.length===0){
+              return null;
+          }
+          return resultado[0] as Vendedor;
+      }
+
+  // agregar
+      async agregarVendedor(vendedor: any): Promise<void> {
+      const query = 'CALL sp_vendedor_crear(?, ?, ?, ?, ?, ?)';
+      const params = [
+        Number(vendedor.dpi),
+        vendedor.nombre,
+        vendedor.apellido,
+        Number(vendedor.telefono),
+        vendedor.correo,
+        vendedor.direccion 
+      ];
+
+      await db.query(query, params);
     }
+  // eliminar
+    async EliminarVendedor(id:number):Promise <void>{
+      const query= 'CALL sp_vendedor_eliminar(?)';
+      await db.query(query,[id]);
 
-// agregar
-    async agregarVendedor(vendedor: any): Promise<void> {
-    const query = 'CALL sp_vendedor_crear(?, ?, ?, ?, ?, ?)';
-    const params = [
-      Number(vendedor.dpi),
-      vendedor.nombre,
-      vendedor.apellido,
-      Number(vendedor.telefono),
-      vendedor.correo,
-      vendedor.direccion 
-    ];
-
-    await db.query(query, params);
-  }
-// eliminar
-  async EliminarVendedor(id:number):Promise <void>{
-    const query= 'CALL sp_vendedor_eliminar(?)';
-    await db.query(query,[id]);
-
-  }
-  // editar
-  async EditarVedendor(id: number,vendedor:Partial<Vendedor>): Promise<void>{
-    const query='CALL sp_vendedor_actualizar(?, ?, ?, ?, ?, ?, ?)'
-    const params=[
-        id,
-      vendedor.dpi,
-      vendedor.nombre,
-      vendedor.apellido,
-      vendedor.telefono,
-      vendedor.correo,
-      vendedor.direccion
-    ];
-    await db.query(query,params);
-  }
-
-
-    
-    
-
-    
+    }
+    // editar
+    async EditarVedendor(id: number,vendedor:Partial<Vendedor>): Promise<void>{
+      const query='CALL sp_vendedor_actualizar(?, ?, ?, ?, ?, ?, ?)'
+      const params=[
+          id,
+        vendedor.dpi,
+        vendedor.nombre,
+        vendedor.apellido,
+        vendedor.telefono,
+        vendedor.correo,
+        vendedor.direccion
+      ];
+      await db.query(query,params);
+    }    
 }
 
 
