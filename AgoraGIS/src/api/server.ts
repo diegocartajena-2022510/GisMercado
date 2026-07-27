9a25a322ed5239fcc95382485bd47d5b6c0d2ded
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-26 18:54:12 -0600

    feat: se importo router de todas las entidades

--- AgoraGIS/src/api/server.ts
+++ AgoraGIS/src/api/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
-import router from './router';
-
+import routerVendedor from './routerVendedor';
+import routerAsignacion from './routerAsignacionP';
+import routerGiroC from './routerGiroC';
+import routerMercado from './routerMercado';
+import routerSector from './routerSector';
+import routerPuesto from './routerPuesto';
+import routerPago from './routerPago';
+import routerUsuario from './routerUsuario';
+import routerMulta from './routerMulta';
+import routerInspeccion from './routerInspeccion';
 export const EmpezarServer = () => {
   const app = express();
 
   app.use(express.json());
 
-  app.use('/api', router);
+  app.use('/api', routerVendedor,routerAsignacion,routerGiroC,routerMercado,
+    routerSector,routerPuesto,routerPago,routerUsuario,routerMulta,routerInspeccion);
 
   const PORT = process.env.PORT || 3000;
 
