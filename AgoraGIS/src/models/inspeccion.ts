35945348f8ebe2ebc0958bd539831b45959bd490
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-26 18:55:23 -0600

    feat: se agrego llaves foraneas

--- AgoraGIS/src/models/inspeccion.ts
+++ AgoraGIS/src/models/inspeccion.ts
@@ -1,10 +1,11 @@
-import { resultado } from "./enumsEntidades";
-
+import { resultado } from "./enums";
 export interface inspeccion {
     id_inspeccion: number;
     fecha_inspeccion: Date;
     resultado: resultado;
     observaciones: string;
+    id_puesto: number;
+    id_usuario:number;
 
 }
 
