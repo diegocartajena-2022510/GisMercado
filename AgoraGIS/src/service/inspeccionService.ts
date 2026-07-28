0242d1318992decd0afcbd556b4d7b36e011d2f6
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-07-27 18:19:35 -0600

    fix: se reparo service incorrecto

--- AgoraGIS/src/service/inspeccionService.ts
+++ AgoraGIS/src/service/inspeccionService.ts
@@ -1,24 +1,50 @@
-import { VendedorRepository } from "../data/vendedorRepository";
-import { Vendedor } from "../models/vendedor";
+import { InspeccionRepository } from "../data/inspeccionRepository";
+import { inspeccion } from "../models/inspeccion";
 
-export class VendedorService {
-    private repo = new VendedorRepository();
+export class InspeccionService {
+    private repo = new InspeccionRepository();
 
-    async listarVendedores() {
+    async ListarInspeccion() {
         return await this.repo.listar();
     }
 
-    async BuscarVendedoresPorId(id: number) {
+    async agregarInspeccion(inspeccion: Partial<inspeccion>) {
+        const campostexto = [
+            { nombre: 'observaciones', valor: inspeccion.observaciones }
+        ];
+
+        for (const campos of campostexto) {
+            if (!campos.valor) {
+                throw new Error(`El campo '${campos.nombre}' no debe ir vacío`);
+            }
+            if (typeof campos.valor !== 'string') {
+                throw new Error(`En el campo '${campos.nombre}' debe ser un texto`);
+            }
+        }
+
+        if (
+            typeof inspeccion.id_puesto !== 'number' || 
+            typeof inspeccion.id_usuario !== 'number' || 
+            isNaN(inspeccion.id_puesto) || 
+            isNaN(inspeccion.id_usuario)
+        ) {
+            throw new Error("El id_puesto o el id_usuario debe ser un número válido");
+        }
+
+        return await this.repo.agregar(inspeccion);
+    }
+
+    async buscarInspeccion(id: number) {
         return await this.repo.buscarPorId(id);
     }
 
-    async agregarVendedores(vendedor: Partial<Vendedor>) {
+    async eliminarInspeccion(id: number) {
+        return await this.repo.eliminar(id);
+    }
+
+    async editarInspeccion(id: number, inspeccion: Partial<inspeccion>) {
         const campostexto = [
-            { nombre: 'nombre', valor: vendedor.nombre },
-            { nombre: 'apellido', valor: vendedor.apellido },
-            { nombre: 'correo', valor: vendedor.correo },
-            { nombre: 'direccion', valor: vendedor.direccion },
-            { nombre: 'telefono', valor: vendedor.telefono }
+            { nombre: 'observaciones', valor: inspeccion.observaciones }
         ];
 
         for (const campos of campostexto) {
@@ -30,65 +56,15 @@ export class VendedorService {
             }
         }
 
-        if (typeof vendedor.dpi !== 'number' || isNaN(vendedor.dpi)) {
-            throw new Error("El DPI debe ser un número válido");
+        if (
+            typeof inspeccion.id_puesto !== 'number' || 
+            typeof inspeccion.id_usuario !== 'number' || 
+            isNaN(inspeccion.id_puesto) || 
+            isNaN(inspeccion.id_usuario)
+        ) {
+            throw new Error("El id_puesto o el id_usuario debe ser un número válido");
         }
 
-        const esGmail = vendedor.correo?.endsWith("@gmail.com");
-        const esHotmail = vendedor.correo?.endsWith("@hotmail.com");
-        const esYahoo = vendedor.correo?.endsWith("@yahoo.com");
-
-        if (!vendedor.correo || (!esGmail && !esHotmail && !esYahoo)) {
-            throw new Error("Correo inválido, solo se permite 'gmail', 'hotmail' y 'yahoo'");
-        }
-
-        const correoExistente = await this.repo.buscarPorCorreo(vendedor.correo);
-        if (correoExistente) {
-            throw new Error("Este correo ya fue registrado, intente de nuevo");
-        }
-
-        return await this.repo.agregarVendedor(vendedor);
-    }
-
-    async editarVendedores(id: number, vendedor: Partial<Vendedor>) {
-        const campostexto = [
-            { nombre: 'nombre', valor: vendedor.nombre },
-            { nombre: 'apellido', valor: vendedor.apellido },
-            { nombre: 'correo', valor: vendedor.correo },
-            { nombre: 'direccion', valor: vendedor.direccion },
-            { nombre: 'telefono', valor: vendedor.telefono }
-        ];
-
-        for (const campos of campostexto) {
-            if (!campos.valor) {
-                throw new Error(`El campo '${campos.nombre}' no debe ir vacío`);
-            }
-            if (typeof campos.valor !== 'string') {
-                throw new Error(`En el campo '${campos.nombre}' debe ser un texto`);
-            }
-        }
-
-        if (typeof vendedor.dpi !== 'number' || isNaN(vendedor.dpi)) {
-            throw new Error("El DPI debe ser un número válido");
-        }
-
-        const esGmail = vendedor.correo?.endsWith("@gmail.com");
-        const esHotmail = vendedor.correo?.endsWith("@hotmail.com");
-        const esYahoo = vendedor.correo?.endsWith("@yahoo.com");
-
-        if (!vendedor.correo || (!esGmail && !esHotmail && !esYahoo)) {
-            throw new Error("Correo inválido, solo se permite 'gmail', 'hotmail' y 'yahoo'");
-        }
-
-        const correoExistente = await this.repo.buscarPorCorreo(vendedor.correo);
-        if (correoExistente && correoExistente.id_vendedor !== id) {
-            throw new Error("Este correo ya pertenece a otro vendedor registrado");
-        }
-
-        return await this.repo.EditarVedendor(id, vendedor);
-    }
-
-    async eliminarVendedores(id: number) {
-        return await this.repo.EliminarVendedor(id);
+        return await this.repo.editar(id, inspeccion);
     }
 }
